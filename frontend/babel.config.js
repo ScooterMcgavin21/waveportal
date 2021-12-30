4f8b8805529764ca0d8b5002a00dc76513f60e07
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-12-30 03:06:29 -0700

    style-components

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,9 +1,10 @@
+
+   
+const plugins = [["babel-plugin-styled-components"],["@babel/transform-runtime"]];
 module.exports = {
   presets: [
     '@babel/preset-env',
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
-  plugins: [
-    ["@babel/transform-runtime"]
-  ]
+  plugins: plugins
 }
